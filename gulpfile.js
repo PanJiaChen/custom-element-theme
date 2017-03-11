1d8fbca0e7f24de84f52d22726d92f06bce7ec22
Pan <panfree23@gmail.com>
2017-03-11 18:47:31 +0800

    update css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ var gulp = require('gulp')
 var cssWrap = require('gulp-css-wrap');
 gulp.task('css-wrap', function() {
   return gulp.src( path.resolve('./theme/index.css'))
-    .pipe(cssWrap({selector:'.my-app'}))
+    .pipe(cssWrap({selector:'.custom-theme'}))
     .pipe(gulp.dest('dist'));
 });
