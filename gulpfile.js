746fe689582847cf8b6519779fe5f16cbc304132
Pan <panfree23@gmail.com>
2017-04-25 16:08:30 +0800

    add clean css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var path = require('path')
 var gulp = require('gulp')
+var cleanCSS = require('gulp-clean-css');
 var cssWrap = require('gulp-css-wrap');
 gulp.task('css-wrap', function() {
   return gulp.src( path.resolve('./theme/index.css'))
     .pipe(cssWrap({selector:'.custom-theme'}))
+    .pipe(cleanCSS())
     .pipe(gulp.dest('dist'));
 });
